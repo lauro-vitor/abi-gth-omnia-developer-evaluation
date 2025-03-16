0e6d189eae7a0ef5b4372a2c6b04b78beb0ef68c
Lauro Vitor Pereira Pina <lauro.pina@globalsys.com.br>
2025-03-15 21:41:23 -0300

    Adding model product rating

---
+++ src/Ambev.DeveloperEvaluation.WebApi/Features/Products/Models/ProductRating.cs
@@ -0,0 +1,8 @@
+﻿namespace Ambev.DeveloperEvaluation.WebApi.Features.Products.Models;
+
+public class ProductRating
+{
+    public decimal Rate { get; set; }
+    public int Count { get; set; }
+}
+
