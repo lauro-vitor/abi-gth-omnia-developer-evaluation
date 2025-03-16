cbe62146c0e9fd5911cc557b479f35dbba1af5b1
Lauro Vitor Pereira Pina <lauro.pina@globalsys.com.br>
2025-03-15 21:38:07 -0300

    Adding create product command handler

---
+++ src/Ambev.DeveloperEvaluation.WebApi/Features/Products/CreateProduct/CreateProductProfile.cs
@@ -0,0 +1,22 @@
+﻿using Ambev.DeveloperEvaluation.Application.Products.CreateProduct;
+using AutoMapper;
+
+namespace Ambev.DeveloperEvaluation.WebApi.Features.Products.CreateProduct;
+
+public class CreateProductProfile : Profile
+{
+    public CreateProductProfile()
+    {
+        CreateMap<CreateProductRequest, CreateProductCommand>()
+            .ForMember(dest => dest.Rate, opt => opt.MapFrom(src => src.Rating.Rate)) 
+            .ForMember(dest => dest.Count, opt => opt.MapFrom(src => src.Rating.Count));
+
+        CreateMap<CreateProductResult, CreateProductResponse>()
+            .ForPath(dest => dest.Rating.Rate, opt => opt.MapFrom(src => src.Rate))
+            .ForPath(dest => dest.Rating.Count,  opt => opt.MapFrom(src => src.Count));
+
+         //.ForMember(dest => dest.Rating.Rate, opt => opt.MapFrom(src => src.Rate))
+         //   .ForMember(dest => dest.Rating.Count, opt => opt.MapFrom(src => src.Count));
+    }
+}
+
