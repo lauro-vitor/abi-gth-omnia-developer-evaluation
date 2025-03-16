72d9547585aec9839b24ac667bca246e01407eed
Lauro Vitor Pereira Pina <lauro.pina@globalsys.com.br>
2025-03-15 21:44:27 -0300

    Adding product controller

---
+++ src/Ambev.DeveloperEvaluation.WebApi/Features/Products/ProductsController.cs
@@ -0,0 +1,93 @@
+﻿using Ambev.DeveloperEvaluation.Application.Products.CreateProduct;
+using Ambev.DeveloperEvaluation.Application.Products.DeleteProduct;
+using Ambev.DeveloperEvaluation.Application.Products.GetAllCategories;
+using Ambev.DeveloperEvaluation.Application.Products.GetByIdProduct;
+using Ambev.DeveloperEvaluation.Application.Products.UpdateProduct;
+using Ambev.DeveloperEvaluation.WebApi.Features.Products.CreateProduct;
+using Ambev.DeveloperEvaluation.WebApi.Features.Products.GetByIdProduct;
+using Ambev.DeveloperEvaluation.WebApi.Features.Products.UpdateProduct;
+using AutoMapper;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Ambev.DeveloperEvaluation.WebApi.Features.Products;
+
+
+[ApiController]
+[Route("api/[controller]")]
+public class ProductsController : Controller
+{
+    private readonly IMediator _mediator;
+    private readonly IMapper _mapper;
+
+    public ProductsController(IMediator mediator, IMapper mapper)
+    {
+        _mediator = mediator;
+        _mapper = mapper;
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Create([FromBody] CreateProductRequest request, CancellationToken cancellationToken)
+    {
+        var command = _mapper.Map<CreateProductCommand>(request);
+
+        var result = await _mediator.Send(command, cancellationToken);
+
+        var response = _mapper.Map<CreateProductResponse>(result);
+
+        return Created(string.Empty, response);
+    }
+
+    [HttpGet("{id}")]
+    public async Task<IActionResult> GetById([FromRoute] int id, CancellationToken cancellationToken)
+    {
+        var command = new GetByIdProductCommand { Id = id };
+
+        var result = await _mediator.Send(command, cancellationToken);
+
+        var response = _mapper.Map<GetByIdProductResponse>(result);
+
+        return Ok(response);
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetAll()
+    {
+        return Ok();
+    }
+
+    [HttpGet("categories")]
+    public async Task<IActionResult> GetAllCategories(CancellationToken cancellationToken)
+    {
+        var command = new GetAllCategoriesCommand();
+
+        var result = await _mediator.Send(command, cancellationToken);
+
+        return Ok(result.Categories);
+    }
+
+    [HttpPut("{id}")]
+    public async Task<IActionResult> Update([FromRoute] int id, [FromBody] UpdateProductRequest request, CancellationToken cancellationToken)
+    {
+        var command = _mapper.Map<UpdateProductCommand>(request);
+
+        command.Id = id;
+
+        var result = await _mediator.Send(command, cancellationToken);
+
+        var response = _mapper.Map<UpdateProductResponse>(result);
+
+        return Ok(response);
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> Delete([FromRoute]int id, CancellationToken cancellationToken)
+    {
+        var command = new DeleteProductCommand { Id = id };
+
+        var result = await _mediator.Send(command, cancellationToken);
+
+        return Ok(result);
+    }
+}
+
