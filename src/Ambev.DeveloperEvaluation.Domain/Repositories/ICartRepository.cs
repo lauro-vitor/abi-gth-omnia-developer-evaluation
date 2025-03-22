892348c5bcb616a12636b8dc4f9b6239685ac43a
Lauro Vitor Pereira Pina <lauro.pina@globalsys.com.br>
2025-03-22 12:36:38 -0300

    Adding Cart repository

---
+++ src/Ambev.DeveloperEvaluation.Domain/Repositories/ICartRepository.cs
@@ -0,0 +1,19 @@
+﻿using Ambev.DeveloperEvaluation.Domain.Entities;
+
+namespace Ambev.DeveloperEvaluation.Domain.Repositories
+{
+    public interface ICartRepository
+    {
+        Task<Cart> CreateAsync(Cart cart, CancellationToken cancellationToken = default);
+
+        Task DeleteAsync(Cart cart, CancellationToken cancellationToken = default);
+
+        Task<Cart?> GetByIdAsync(int id, CancellationToken cancellationToken = default);
+
+        Task<Cart?> GetByIdAsNoTrackingAsync(int id, CancellationToken cancellationToken = default);
+
+        IQueryable<Cart> GetAll();
+
+        Task<Cart> UpdateAsync(Cart cart, CancellationToken cancellationToken = default);
+    }
+}
---
+++ src/Ambev.DeveloperEvaluation.ORM/Repositories/CartRepository.cs
@@ -0,0 +1,57 @@
+﻿using Ambev.DeveloperEvaluation.Domain.Entities;
+using Ambev.DeveloperEvaluation.Domain.Repositories;
+using Microsoft.EntityFrameworkCore;
+
+namespace Ambev.DeveloperEvaluation.ORM.Repositories
+{
+    public class CartRepository : ICartRepository, IDisposable
+    {
+        private readonly DefaultContext _context;
+
+        public CartRepository(DefaultContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Cart> CreateAsync(Cart cart, CancellationToken cancellationToken = default)
+        {
+            await _context.AddAsync(cart, cancellationToken);
+            await _context.SaveChangesAsync(cancellationToken);
+            return cart;
+        }
+
+        public async Task DeleteAsync(Cart cart, CancellationToken cancellationToken = default)
+        {
+            _context.Remove(cart);
+            await _context.SaveChangesAsync(cancellationToken);
+        }
+
+        public IQueryable<Cart> GetAll()
+        {
+            return _context.Carts.AsNoTracking();
+        }
+
+        public async Task<Cart?> GetByIdAsync(int id, CancellationToken cancellationToken = default)
+        {
+            return await _context.Carts.FirstOrDefaultAsync(c => c.Id == id, cancellationToken);
+        }
+
+        public async Task<Cart?> GetByIdAsNoTrackingAsync(int id, CancellationToken cancellationToken = default)
+        {
+            return await _context.Carts.AsNoTracking().FirstOrDefaultAsync(c => c.Id == id, cancellationToken);
+        }
+
+        public async Task<Cart> UpdateAsync(Cart cart, CancellationToken cancellationToken = default)
+        {
+            _context.Update(cart);
+            await _context.SaveChangesAsync(cancellationToken);
+            return cart;
+        }
+
+        public void Dispose()
+        {
+            _context?.Dispose();
+            GC.SuppressFinalize(this);
+        }
+    }
+}
