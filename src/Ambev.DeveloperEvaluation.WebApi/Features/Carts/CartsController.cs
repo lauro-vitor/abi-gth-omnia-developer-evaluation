b2b6b642d0d2409ccb0cefe6802c338118715957
Lauro Vitor Pereira Pina <lauro.pina@globalsys.com.br>
2025-03-22 15:39:22 -0300

    Adding endpoint update Cart

--- src/Ambev.DeveloperEvaluation.WebApi/Features/Carts/CartsController.cs
+++ src/Ambev.DeveloperEvaluation.WebApi/Features/Carts/CartsController.cs
@@ -1,6 +1,8 @@
 ﻿using Ambev.DeveloperEvaluation.Application.Carts.CreateCart;
+using Ambev.DeveloperEvaluation.Application.Carts.UpdateCart;
 using Ambev.DeveloperEvaluation.WebApi.Common;
 using Ambev.DeveloperEvaluation.WebApi.Features.Carts.CreateCart;
+using Ambev.DeveloperEvaluation.WebApi.Features.Carts.UpdateCart;
 using AutoMapper;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
@@ -10,12 +12,12 @@ namespace Ambev.DeveloperEvaluation.WebApi.Features.Carts
 
     [ApiController]
     [Route("api/[controller]")]
-    public class CartController : BaseController
+    public class CartsController : BaseController
     {
         private readonly IMediator _mediator;
         private readonly IMapper _mapper;
 
-        public CartController(IMediator mediator, IMapper mapper)
+        public CartsController(IMediator mediator, IMapper mapper)
         {
             _mediator = mediator;
             _mapper = mapper;
@@ -26,10 +28,27 @@ namespace Ambev.DeveloperEvaluation.WebApi.Features.Carts
         public async Task<IActionResult> Create([FromBody] CreateCartRequest request)
         {
             var command = _mapper.Map<CreateCartCommand>(request);
+
             var result = await _mediator.Send(command);
+
             var response = _mapper.Map<CreateCartResponse>(result);
 
             return Created(string.Empty, response);
+        }
+
+        [HttpPut("{id}")]
+        [ProducesResponseType(typeof(UpdateCartResponse), StatusCodes.Status200OK)]
+        public async Task<IActionResult> Update([FromRoute] int id, [FromBody] UpdateCartRequest request)
+        {
+            var command = _mapper.Map<UpdateCartCommand>(request);
+
+            command.CartId = id;
+
+            var result = await _mediator.Send(command);
+
+            var response = _mapper.Map<UpdateCartResponse>(result);
+
+            return Ok(response);
         }
     }
 }
