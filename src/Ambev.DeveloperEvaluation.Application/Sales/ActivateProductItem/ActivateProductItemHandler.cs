029bae4854b85eedc45cc6e267ce0ae1f012646d
Lauro Vitor Pereira Pina <lauro.pina@globalsys.com.br>
2025-03-23 15:52:33 -0300

    Removing unnecessary using

--- src/Ambev.DeveloperEvaluation.Application/Sales/ActivateProductItem/ActivateProductItemHandler.cs
+++ src/Ambev.DeveloperEvaluation.Application/Sales/ActivateProductItem/ActivateProductItemHandler.cs
@@ -2,12 +2,6 @@
 using Ambev.DeveloperEvaluation.Domain.Repositories;
 using AutoMapper;
 using MediatR;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using static Microsoft.EntityFrameworkCore.DbLoggerCategory.Database;
 
 namespace Ambev.DeveloperEvaluation.Application.Sales.ActivateProductItem
 {
