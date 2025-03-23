52cfaba66d7ba550087d28348a026838046030de
Lauro Vitor Pereira Pina <lauro.pina@globalsys.com.br>
2025-03-23 20:37:14 -0300

    Adding errror midle handler

---
+++ src/Ambev.DeveloperEvaluation.WebApi/Middleware/ErrorHandlingMiddleware.cs
@@ -0,0 +1,61 @@
+﻿using System.Net;
+using System.Text.Json;
+
+namespace Ambev.DeveloperEvaluation.WebApi.Middleware
+{
+    public class ErrorHandlingMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public ErrorHandlingMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch (Exception ex)
+            {
+                await HandleExceptionAsync(context, ex);
+            }
+        }
+
+        private static Task HandleExceptionAsync(HttpContext context, Exception exception)
+        {
+            var errorResponse = exception switch
+            {
+                KeyNotFoundException => new ErrorResponse("ResourceNotFound", "Resource not found", exception.Message, HttpStatusCode.NotFound),
+                UnauthorizedAccessException => new ErrorResponse("AuthenticationError", "Unauthorized", exception.Message, HttpStatusCode.Unauthorized),
+                ArgumentException => new ErrorResponse("ValidationError", "Invalid input data", exception.Message, HttpStatusCode.BadRequest),
+                _ => new ErrorResponse("ServerError", "An unexpected error occurred", exception.Message, HttpStatusCode.InternalServerError)
+            };
+
+            var response = JsonSerializer.Serialize(errorResponse);
+            context.Response.ContentType = "application/json";
+            context.Response.StatusCode = (int)errorResponse.StatusCode;
+
+            return context.Response.WriteAsync(response);
+        }
+    }
+
+    public class ErrorResponse
+    {
+        public string Type { get; }
+        public string Error { get; }
+        public string Detail { get; }
+        public HttpStatusCode StatusCode { get; }
+
+        public ErrorResponse(string type, string error, string detail, HttpStatusCode statusCode)
+        {
+            Type = type;
+            Error = error;
+            Detail = detail;
+            StatusCode = statusCode;
+        }
+    }
+
+}
--- src/Ambev.DeveloperEvaluation.WebApi/Program.cs
+++ src/Ambev.DeveloperEvaluation.WebApi/Program.cs
@@ -54,6 +54,7 @@ public class Program
 
             var app = builder.Build();
             app.UseMiddleware<ValidationExceptionMiddleware>();
+            app.UseMiddleware<ErrorHandlingMiddleware>();
 
             if (app.Environment.IsDevelopment())
             {
